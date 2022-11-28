50547b537eceb7a5d3a6b238fb02d932351dd784
cloud <cloud@secure-workflow-tutorial.novalocal>
2022-11-28 13:38:58 +0100

    Change path to utils directory, add placeholder for data dir

--- client/automatic.sh
+++ client/automatic.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Prepare input data container
-echo "YES" | ../utils/create_data_container.sh inputdata /mnt 50
+echo "YES" | utils/create_data_container.sh inputdata /mnt 50
 # Copies all data files into the inputdata folder
-cp ../data/* /mnt/inputdata/							
+cp <path-to-data-dir>/* /mnt/inputdata/							
 # Verifies data has been copied
 ls /mnt/inputdata/								
 
 # Verifies data has been copied
-../utils/umount_data_container.sh inputdata /mnt				
+utils/umount_data_container.sh inputdata /mnt				
 scp inputdata.img <hpc-uid>@<hpc-frontend>:/scratch/users/<hpc-uid>/secure
 
 # Secure-copies inputdata.img onto hpc server
-scp inputdata.img <<hpc-uid>@<hpc-frontend>:/scratch/users/<hpc-uid>/secure	
+scp inputdata.img <hpc-uid>@<hpc-frontend>:/scratch/users/<hpc-uid>/secure	
 
 # Prepare output container
-echo "YES" | ../utils/create_data_container.sh outdata /mnt 500			
+echo "YES" | utils/create_data_container.sh outdata /mnt 500			
 # Unmounts
-../utils/umount_data_container.sh outdata /mnt					
+utils/umount_data_container.sh outdata /mnt					
 
 # Copies outdata.img onto hpc server
 scp outdata.img <hpc-uid>@<hpc-frontend>:/scratch/users/<hpc-uid>/secure	
@@ -27,7 +27,7 @@ scp outdata.img <hpc-uid>@<hpc-frontend>:/scratch/users/<hpc-uid>/secure
 ./prepare_scripts.sh
 
 # Creates command.sh.asc, recipient is slurm 
-../utils/encrypt_script.sh command.sh <pubKeyOfServer>
+utils/encrypt_script.sh command.sh <pubKeyOfServer>
 
 cat command.sh.asc
 
@@ -49,7 +49,7 @@ gpg --detach-sign --local-user <LocalUserKey> -o run.sh.sig run.sh
 scp run.sh run.sh.sig <hpc-uid>@<hpc-frontend>:/scratch/users/<hpc-uid>/home/
 
 # Executes run.sh on secure client
-slurmid=$(ssh <hpc-uid>@<hpc-frontend> 'cd /scratch/users/sachut<>?/home/ ; /opt/slurm/bin/sbatch -p secure -G 1 --time=2-00:00:00 -n 16 --mem=40G run.sh' | gawk '{print $4}')
+slurmid=$(ssh <hpc-uid>@<hpc-frontend> 'cd /scratch/users/<hpc-uid>/home/ ; /opt/slurm/bin/sbatch -p secure -G 1 --time=2-00:00:00 -n 16 --mem=40G run.sh' | gawk '{print $4}')
 
 while [[ $(ssh <hpc-uid>@<hpc-frontend> "/opt/slurm/bin/sacct -b -j $slurmid | gawk '{print $2}' | awk 'FNR > 2' | grep -v COMPLETED") != "" ]] ; do echo "Waiting for job to end!" ; sleep 10 ; done
 
@@ -59,4 +59,4 @@ echo 'finished'
 scp <hpc-uid>@<hpc-frontend>:/scratch/users/<hpc-uid>/secure/outdata.img .
 
 # Mounts outdata
-../utils/mount_data.sh outdata
+utils/mount_data.sh outdata
