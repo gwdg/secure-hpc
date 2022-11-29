4d884919d069d5c91fb4c42e7df4363d7d7d9dd4
cloud <cloud@secure-workflow-tutorial.novalocal>
2022-11-29 11:56:17 +0100

    Make changes to automatic.sh from client for tutorial

--- tutorial/JobTemplate/automatic.sh
+++ tutorial/JobTemplate/automatic.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
 # Prepare input data container
-echo "YES" | ../utils/create_data_container.sh inputdata /mnt 50
-# Copies all data files into the inputdata folder
-cp ../data/* /mnt/inputdata/							
+echo "YES" | utils/create_data_container.sh inputdata /mnt 50
+cp <path-to-data-dir>/* /mnt/inputdata/
 # Verifies data has been copied
-ls /mnt/inputdata/								
-
-# Verifies data has been copied
-../utils/umount_data_container.sh inputdata /mnt				
-scp inputdata.img hnolte1@transfer-scc.gwdg.de:/scratch/users/hnolte1/secure
-
-# Secure-copies inputdata.img onto hpc server
-scp inputdata.img sachut@transfer-scc.gwdg.de:/scratch/users/sachut/secure	
+ls /mnt/inputdata/
+# Unmounts input data container
+<utils-dir>/umount_data_container.sh inputdata /mnt
+# Copies inputdata.img onto hpc server
+scp inputdata.img <hpc-uid>@transfer-scc.gwdg.de:/scratch/users/<hpc-uid>/secure
 
 # Prepare output container
-echo "YES" | ../utils/create_data_container.sh outdata /mnt 500			
+echo "YES" | ../utils/create_data_container.sh outdata /mnt 500
 # Unmounts
-../utils/umount_data_container.sh outdata /mnt					
-
+<utils-dir>/umount_data_container.sh outdata /mnt	
+# Create a directory on the server
+ssh <hpc-uid>@gwdu101.gwdg.de 'mkdir /scratch/users/<hpc-uid>/secure'
 # Copies outdata.img onto hpc server
-scp outdata.img sachut@transfer-scc.gwdg.de:/scratch/users/<hpc-uid>/secure	
+scp outdata.img <hpc-uid>@transfer-scc.gwdg.de:/scratch/users/<hpc-uid>/secure
 
 # Prepares command.sh, copies generated default and wrapped tokens into it
 # Sends keys to vaults
 ./prepare_scripts.sh
 
 # Creates command.sh.asc, recipient is slurm 
-../utils/encrypt_script.sh command.sh <pubKeyOfServer>
+<utils-dir>/encrypt_script.sh command.sh <pubKeyOfServer>
 
 cat command.sh.asc
 
@@ -46,17 +43,17 @@ cat run.sh
 gpg --detach-sign --local-user <LocalUserKey> -o run.sh.sig run.sh
 
 # Secure copies run.sh, run.sh.sig onto scratch 
-scp run.sh run.sh.sig <hpc-uid>@<hpc-frontend>:/scratch/users/sachut/home/
+scp run.sh run.sh.sig <hpc-uid>@transfer-scc.gwdg.de:/scratch/users/<hpc-uid>/home/
 
 # Executes run.sh on secure client
-slurmid=$(ssh <hpc-uid>@<hpc-frontend> 'cd /scratch/users/sachut/home/ ; /opt/slurm/bin/sbatch -p secure -G 1 --time=2-00:00:00 -n 16 --mem=40G run.sh' | gawk '{print $4}')
+slurmid=$(ssh <hpc-uid>@gwdu101.gwdg.de 'cd /scratch/users/<hpc-uid>/home/ ; /opt/slurm/bin/sbatch -p secure -G 1 --time=2-00:00:00 -n 16 --mem=40G run.sh' | gawk '{print $4}')
 
-while [[ $(ssh <hpc-uid>@<hpc-frontend> "/opt/slurm/bin/sacct -b -j $slurmid | gawk '{print $2}' | awk 'FNR > 2' | grep -v COMPLETED") != "" ]] ; do echo "Waiting for job to end!" ; sleep 10 ; done
+while [[ $(ssh <hpc-uid>@gwdu101.gwdg.de "/opt/slurm/bin/sacct -b -j $slurmid | gawk '{print $2}' | awk 'FNR > 2' | grep -v COMPLETED") != "" ]] ; do echo "Waiting for job to end!" ; sleep 10 ; done
 
 echo 'finished'
 
 # Copies image file from server to local 
-scp <hpc-uid>@<hpc-frontend>:/scratch/users/sachut/secure/outdata.img .
+scp <hpc-uid>@transfer-scc.gwdg.de:/scratch/users/<hpc-uid>/secure/outdata.img .
 
 # Mounts outdata
-../utils/mount_data.sh outdata
+<utils-dir>/mount_data.sh outdata
